3c5ceb9a319ae1ab84df7b275815bb11a797b440
Abhishek069 <abhishinde0723@gmail.com>
2025-12-12 19:08:24 +0530

    updat the color for the live section

--- src/components/JodiPannelResultSection.jsx
+++ src/components/JodiPannelResultSection.jsx
@@ -156,7 +156,6 @@ export default function JodiPannelResultSection() {
   // The function that calls backend
   const handleDeleteRecord = async () => {
 
-    
     if (!deleteGameId) {
       toast.error("Select a game");
       return;
--- src/components/LiveResultItem.jsx
+++ src/components/LiveResultItem.jsx
@@ -3,12 +3,12 @@ import React from "react";
 
 const LiveResultItem = ({ title, numbers, openTime, closeTime }) => {
   return (
-    <div className="text-center mb-4 m-1 Live-result-item-main-container bg-[#ffea00]">
+    <div className="text-center mb-4 m-1 Live-result-item-main-container ">
       <h5 className="text-uppercase fw-bold LiveResultItemHeading">{title}</h5>
       <p className="fs-5 fw-semibold  LiveResultItemHNumber">{numbers}</p>
       <div className="live-times">
-        <span>{openTime}</span>
-        <span>{closeTime}</span>
+        <span className="live-times-value">{openTime}</span>
+        <span className="live-times-value">{closeTime}</span>
       </div>
 
       <button
